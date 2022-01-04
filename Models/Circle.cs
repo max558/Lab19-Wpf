a9ef6217d0e0b8a71f6f24e837527dfa139cf7a6
mivvlk <mivvlk@yandex.ru>
2022-01-04 21:23:50 +0300

    Добавьте файлы проекта.

---
+++ Models/Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19___WpfApp.Models
+{
+    //Класс из урока 12 (основы C++)
+    static class Circle
+    {
+        public static double LengthCircle(double r)
+        {
+            double res = 0;
+            res = 2 * Math.PI * r;
+            return res;
+        }
+        public static double SquareCircle(double r)
+        {
+            double res = 0;
+            res = Math.PI * Math.Pow(r, 2);
+            return res;
+        }
+        public static void ComparPointCircle(int pointX, int pointY, double circleR, int centrPointCircleX = 0, int centrPointCircleY = 0)
+        {
+            //Определяем длину от центра окружности до точки
+            double L = Math.Sqrt(Math.Pow((centrPointCircleX - pointX), 2) + Math.Pow((centrPointCircleY - pointY), 2));
+            string str = "";
+            if (L <= circleR)
+            {
+                str = "принадлежит";
+            }
+            else
+            {
+                str = "не принадлежит";
+            }
+
+            Console.WriteLine(
+                "Точка с координатами ({0};{1}) {4} окружности с радиусом {5:f2} и с центром в точке ({2};{3})",
+                pointX, pointY, centrPointCircleX, centrPointCircleY, str, circleR);
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Lab19___WpfApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab19___WpfApp.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged ([CallerMemberName]string propertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private double numRadius;
+        public double NumRadius
+        {
+            get => numRadius;
+            set
+            {
+                numRadius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double circumference;
+        public double Circumference
+        {
+            get => circumference;
+            set
+            {
+                circumference = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double areaCircle;
+        public double AreaCircle
+        {
+            get => areaCircle;
+            set
+            {
+                areaCircle = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand CalcCommand { get; }
+        private void OnCalcCommandExecute (object p)
+        {
+            Circumference = Circle.LengthCircle(numRadius);
+            AreaCircle = Circle.SquareCircle(numRadius);
+        }
+
+        public MainWindowViewModel()
+        {
+            CalcCommand = new RelayCommand(OnCalcCommandExecute, null) ;
+        }
+    }
+}
